0726ff619d9e97cdfaab6208a9bd674e74164873
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-17 17:31:07 +0100

    refactored the seeder to take in all of its data from the CLI

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
@@ -17,20 +18,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
 
-        for($i=0; $i<500; $i++) {
+        $amountToCreate = $this->command->getOutput()->ask("How Many Users Should The Seeder Create",500);
+
+        $passToSet = (string)$this->command->ask("What Should The Password Be", "12345678");
+
+        $faker = Factory::create();
+
+        $this->command->getOutput()->progressStart($amountToCreate);
+
+        for($i=0; $i<$amountToCreate; $i++) {
 
             $username="user_" . $faker->userName;
-            $password =$faker->password;
-            $email=$faker->safeEmail;
+            $password = $passToSet;
+            $email=$faker->safeEmail();
 
             User::create([
                 'name' => $username,
                 "email" => $email,
-                "password" => Hash::make($password)
+                "password" => $password
             ]);
+            $this->command->getOutput()->progressAdvance(1);
         }
+        $this->command->getOutput()->progressFinish();
+
 
 
     }
