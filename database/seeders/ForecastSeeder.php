6d94d4b15d2f2356042a225650a7ceb77fead681
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-21 11:49:03 +0100

    now the forecast seeder does not allow the temp from day to day to differ more then 5 deegrees

--- database/seeders/ForecastSeeder.php
+++ database/seeders/ForecastSeeder.php
@@ -15,6 +15,7 @@ class ForecastSeeder extends Seeder
      *
      * @return void
      */
+    //WHEN RAN, IT WILL SEED TODAY AND THE NEXT 6 DAYS
     public function run()
     {
         //important: for now the paths and the descriptions need to be in symmetric order so there is not a discrepancy in the frontend!
@@ -35,26 +36,28 @@ class ForecastSeeder extends Seeder
 
         $startIndex = CityModel::first()->id;
         $endIndex = CityModel::all()->last()->id;
-
-        //loop through all cities
-        //for all purposes, i is the id of the city we are referencing, j counts how many entries are entered
-        $this->command->getOutput()->progressStart();
-        for($i=$startIndex; $i<=$endIndex; $i++){
-            //5 temps for each city with a random date need to be generated
+        //$this->command->getOutput()->progressStart();
+        for($i=$startIndex; $i<$endIndex; $i++){
+            $temperature = null;
             for($j=0; $j<=6; $j++){
-             $temperature = rand($temperatureRange[0],$temperatureRange[1]);
-             $indexOfImagePathAndDescription = WeatherHelper::descriptionDeterminer($temperature);
-             ForecastModel::create([
-                 "city_id"=>$i,
-                 "temperature"=> $temperature,
-                 "date"=>Carbon::now()->addDays($j),
-                 "description"=>$descriptions[$indexOfImagePathAndDescription],
-                 "probability"=>WeatherHelper::returnProbability($indexOfImagePathAndDescription),
-                 "path_to_image"=>$pathsToImages[$indexOfImagePathAndDescription]
-             ]);
-             $this->command->getOutput()->progressAdvance(1);
+                if($temperature===null){
+                    $temperature=rand($temperatureRange[0],$temperatureRange[1]);
+                }else{
+                    $temperature=rand($temperature-5,$temperature+5);
+                }
+
+                ForecastModel::create([
+                    "city_id"=>$i,
+                    "temperature"=>$temperature,
+                    "date"=>Carbon::now()->addDays($j),
+                    "description"=>$descriptions[WeatherHelper::descriptionDeterminer($temperature)],
+                    "probability"=>WeatherHelper::returnProbability($descriptions[WeatherHelper::descriptionDeterminer($temperature)]),
+                    "path_to_image"=>WeatherHelper::determinePathToImage($descriptions[WeatherHelper::descriptionDeterminer($temperature)])
+                ]);
+
             }
         }
-        $this->command->getOutput()->progressFinish();
+
+
     }
 }
