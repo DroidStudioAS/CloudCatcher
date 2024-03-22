a05fa063704fd3d807e519ef3950667ee09441e1
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-22 09:32:30 +0100

    changed auth redirect to /weather insted of home

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -26,7 +26,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/weather";
 
     /**
      * Create a new controller instance.
