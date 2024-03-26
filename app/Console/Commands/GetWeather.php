e8d12249d7cfc69acc8b2dda283501534324ea9d
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-26 10:38:41 +0100

    added error checking and argument to getweather command

--- app/Console/Commands/GetWeather.php
+++ app/Console/Commands/GetWeather.php
@@ -12,7 +12,7 @@ class GetWeather extends Command
      *
      * @var string
      */
-    protected $signature = 'weather:get';
+    protected $signature = 'weather:get {city}';
 
     /**
      * The console command description.
@@ -44,14 +44,19 @@ class GetWeather extends Command
 
        $url = "http://api.weatherapi.com/v1/current.json";
        $key = "21484ee16dbe4b5a86593731231207";
-       $city = "Belgrade";
+       $city = $this->argument("city");
        $response = Http::get($url,[
            "key"=>$key,
            "q"=>$city
        ]);
 
+       $code = $response->status();
        $json_response = json_decode($response->body(), true);
-       dd($json_response["location"]["name"] . ":" . $json_response["current"]["temp_c"] . " Celsius");
+       if(isset($json_response["error"])){
+           $this->getOutput()->error($json_response["error"]["message"]);
+           exit();
+       }
+        $this->getOutput()->writeln($json_response["location"]["name"] . ":" . $json_response["current"]["temp_c"] . " Celsius");
 
 
 
