541addb061359c24e941f26522ba84305b634b9f
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-26 07:13:42 +0100

    added backend method for adding a city to favourites

--- app/Http/Controllers/UserCitiesController.php
+++ app/Http/Controllers/UserCitiesController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CityModel;
+use App\Models\UserCity;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserCitiesController extends Controller
 {
-    public function addToFavorites($city){
+    public function addToFavorites(CityModel $city){
+        //fetch userid (route protected by middelware, so user ID should never be null)
+        $userId = Auth::id();
+        //JUST IN CASE
+        if($userId===null || $city===null){
+            return response([
+                "success"=>false
+            ]);
+        }
+        //add city
+        UserCity::create([
+            "user_id"=>$userId,
+            "city_id"=>$city->id
+        ]);
+        //return true
         return response([
             "success"=>true
         ]);
